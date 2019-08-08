958cbf8db590102c6edba4ebd4ae95843897da5b
ecwing <ericcwing@gmail.com>
2019-08-08 15:52:52 -0400

    JavaScript functionality WIP

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+let display = document.getElementById("display")
+let buttons = document.getElementsByClassName("btn");
+
+for (let i=0; i < buttons.length; i++) {
+   let button = buttons[i];
+}
+
+switch (button.classList.contains("actions")) {
+    case true
+    
+}
+
+let number = document.getElementById("number")
