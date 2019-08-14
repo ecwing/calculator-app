200954c4638fc5fbe3a998350840efa8c308fbf2
ecwing <ericcwing@gmail.com>
2019-08-13 22:22:55 -0400

    calculator functionality complete! ::rocket::

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,47 @@ for (let i=0; i < buttons.length; i++) {
    let button = buttons[i];
 }
 
-switch (button.classList.contains("actions")) {
-    case true
-    
-}
+    button.addEventListener("click", function () {
+        switch (button.classList.contains("actions")) {
+            case true:
+                if (button.classList.contains("operator")) {
+                    concatOperation(button)
+                } else {
+                    callAction(button);
+                }
+                break;
+            case false:
+                        concatOperation(button);
+                break;
+        }
 
-let number = document.getElementById("number")
+    })
+
+function concatOperation (button) {
+    if (button.classList.contains("operator")) {
+        if (!operation) {
+            return;
+        } 
+    }  
+    operation += button.dataset.value;
+
+    display.innerHTML = operation;
+}    
+
+function callAction(button) {
+    switch (button.dataset.value) {
+        case "calculate":
+            operation = eval(operation).toString();
+            display.innerHTML = operation;
+            break;
+        case "backspace":
+            operation = operation.slice(0, -1);
+            display.innerHTML = operation;
+            break;
+        case "clear":
+            operation = "";
+            display.innerHTML = "";
+            break;
+    }
+
+}
